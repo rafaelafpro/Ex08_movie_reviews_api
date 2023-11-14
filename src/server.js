f062c8b10194a03221c813ffbdbec67fba108c6a
Rafael <rafaelafpro@gmail.com>
2023-11-14 13:19:18 -0300

    Stage 10 2

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require('express-async-errors');
 const migrationsRun = require('./database/sqlite/migrations');
 const AppError = require('./utils/AppError');
 const uploadConfig = require("./configs/upload");
+const cors = require("cors");
+
 
 const express = require('express');
 const routes = require('./routes');
@@ -9,6 +11,7 @@ const routes = require('./routes');
 migrationsRun();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
